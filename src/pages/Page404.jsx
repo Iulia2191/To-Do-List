c9e97a582283a09a7a40bf83a41e73d9c8e84dac
Iulia2191 <125976840+Iulia2191@users.noreply.github.com>
2023-07-06 17:52:09 +0200

    color

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -1,10 +1,10 @@
 import { Link } from 'react-router-dom'
+import styles from '../pages/page404.module.css'
 
 export function Page404 () {
   return (
-    <div>
-      <h1>Page404</h1>
-      <Link to='/'>Home</Link>
+    <div className={styles.body}>
+      <h1 className='my-5'>Page404</h1>
     </div>
   )
 }
