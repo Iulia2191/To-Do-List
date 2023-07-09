867efc044e94ac6e6d1230b1dc0d6c269bd818e1
Iulia2191 <125976840+Iulia2191@users.noreply.github.com>
2023-07-09 12:09:53 +0200

    css

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -11,39 +11,41 @@ export default function Contact () {
   console.log(errors)
 
   return (
-    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-      <h1 className={styles.title}>Contact Us</h1>
-      <h5 className='text-center my-5'>
-        If you have questions or you want to leave us a message, please fill out
-        our form.
-      </h5>
-      <input
-        className={styles.input}
-        type='text'
-        placeholder='First name'
-        {...register('firstName', { required: true, maxLength: 80 })}
-      />
-      {errors.firstName && <span>This field is required.</span>}
+    <div className='container-fluid d-flex justify-content-center'>
+      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+        <h1 className={styles.title}>Contact Us</h1>
+        <h5 className='text-center my-5'>
+          If you have questions or you want to leave us a message, please fill
+          out our form.
+        </h5>
+        <input
+          className={styles.input}
+          type='text'
+          placeholder='First name'
+          {...register('firstName', { required: true, maxLength: 80 })}
+        />
+        {errors.firstName && <span>This field is required.</span>}
 
-      <input
-        className={styles.input}
-        type='text'
-        placeholder='Last name'
-        {...register('lastName', { required: true, maxLength: 100 })}
-      />
-      {errors.lastName && <span>This field is required.</span>}
+        <input
+          className={styles.input}
+          type='text'
+          placeholder='Last name'
+          {...register('lastName', { required: true, maxLength: 100 })}
+        />
+        {errors.lastName && <span>This field is required.</span>}
 
-      <input
-        className={styles.input}
-        type='text'
-        placeholder='Email'
-        {...register('email', { required: true, pattern: /^\S+@\S+$/i })}
-      />
-      {errors.email && <span>Please enter a valid email address.</span>}
+        <input
+          className={styles.input}
+          type='text'
+          placeholder='Email'
+          {...register('email', { required: true, pattern: /^\S+@\S+$/i })}
+        />
+        {errors.email && <span>Please enter a valid email address.</span>}
 
-      <textarea className={styles.input} {...register('message')} />
+        <textarea className={styles.input} {...register('message')} />
 
-      <input className={styles.button} type='submit' />
-    </form>
+        <input className={styles.button} type='submit' />
+      </form>
+    </div>
   )
 }
