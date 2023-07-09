0d4dee78b8a1b876bd3b7eca15fdf720baf0ff7c
Iulia2191 <125976840+Iulia2191@users.noreply.github.com>
2023-07-09 11:20:41 +0200

    style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,26 +11,24 @@ export function Home () {
       <h4 className='text-center'>Conquer Your To-Do List</h4>
       <div className={styles.container}>
         <div>
-          <p className={styles.paragraf}>
+          <p className={styles.paragraf1}>
             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
             nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
             erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
             et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
             Lorem ipsum dolor sit amet.
           </p>
-          <p className={styles.paragraf}>
+          <p className={styles.paragraf1}>
             Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse
             molestie consequat, vel illum dolore eu feugiat nulla facilisis at
             vero eros et accumsan et iusto odio dignissim qui blandit praesent
             luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
           </p>
-          <p className={styles.paragraf}>
+          <p className={styles.paragraf1}>
             Sed ut perspiciatis unde omnis iste natus error sit voluptatem
             accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
             quae ab illo inventore veritatis et quasi architecto beatae vitae
-            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-            eos qui ratione voluptatem sequi nesciunt.
+            dicta sunt explicabo.
           </p>
         </div>
         <div>
